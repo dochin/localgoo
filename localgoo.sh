67e736914749baa94a34531ca4efdeb0d05e5313
Jason Campbell <dochin@campound.org>
2014-12-29 09:55:19 -0500

    Better behavior on update

--- localgoo.sh
+++ localgoo.sh
@@ -21,13 +21,17 @@ gooinit() {
 	if [ -f "$CONFIG_FILE" ]; then
 		echo "Previous configuration exists. Try ./localgoo.sh update or remove $CONFIG_FILE."
 	elif [ "$SITE_ADDR" == "" ]; then
-		echo "Usage: ./localgoo.sh init|update|download|danger [/path/to/website] [yourwebsite.com]"
+		echo "Usage: ./localgoo.sh init [/path/to/website] [yourwebsite.com]"
 	else
 		# Create public html directory to store localgoo downloaded files
-		echo "Creating new public_html directory."
-		mkdir $PUBLIC_HTML
-		echo '<head></head><body><h1>localgoo</h1><h2>This site uses localgoo to protect your privacy.  Details at <a href=https://github.com/dochin/localgoo/>https://github.com/dochin/localgoo</a></h2></body>' > $PUBLIC_HTML/index.html
-
+		if [ -d "$PUBLIC_HTML" ]; then
+			echo "Using existing $PUBLIC_HTML directory."
+		else
+			echo "Creating new $PUBLIC_HTML directory."
+			mkdir $PUBLIC_HTML
+			echo '<head></head><body><h1>localgoo</h1><h2>This site uses localgoo to protect your privacy.  Details at <a href=https://github.com/dochin/localgoo/>https://github.com/dochin/localgoo</a></h2></body>' > $PUBLIC_HTML/index.html
+		fi
+		
 		#Save config
 		mkdir -p "$CONFIG_DIR"
 		echo "SITE_ADDR=$SITE_ADDR" > $CONFIG_FILE
@@ -41,7 +45,7 @@ gooinit() {
 }
 
 goobackup() {
-	# Create an initial backup of the existing site 
+	# Create a backup of the existing site 
 	if [ -d "$SEARCH_DIR/.git" ]; then
 		git --git-dir="$SEARCH_DIR/.git" --work-tree="$SEARCH_DIR" add .
 		git --git-dir="$SEARCH_DIR/.git" --work-tree="$SEARCH_DIR" commit -a -m "Pre-localgoo backup."
@@ -53,16 +57,13 @@ goobackup() {
 }
 
 goosearch() {
-	echo "Searching for calls to google apis..."
+	echo "Searching for calls to google apis."
 	
 	#find all the files that are infected with google api calls
 	grep -r -E "('|\"|\()(https://|http://|://|//)?(www|fonts|ajax)\.googleapis" "$SEARCH_DIR" > found.tmp
 
 	#make a list of the files to change based on the grep output
 	sed 's/:.*$//' <found.tmp >files.tmp
-	
-	#remove duplicates
-	sort -u files.tmp >unique.tmp
 
 	#generate urls to download
 	grep -E -o "('|\"|\()(https://|http://|://|//)?(www|fonts|ajax)\.googleapis\.com[^ ;]*('|\"|\))" found.tmp > download.tmp
@@ -87,25 +88,28 @@ goosearch() {
 	sed -i '/\$font\.$/ d' download.tmp
 	sed -i '/&subset=$subsets/ d' download.tmp
 	
-	#Add extra fonts from extrafonts.txt and ignore comment lines
-	sed -e 's@^@https://fonts.googleapis.com/css?family=@' -e '/^#/ d' extrafonts.txt >> download.tmp
+	#Add extra fonts from extrafonts.list and ignore comment lines
+	sed -e 's@^@https://fonts.googleapis.com/css?family=@' -e 's@#.*$@@' extrafonts.list >> download.tmp
 	
 	#Replace spaces with %20
 	sed -i 's@ @%20@ g' download.tmp
 
-	#Remove duplicates
-	sort -u download.tmp > download2.tmp
-	mv download2.tmp download.tmp
+	#sort and remove dupes/append the download and files lists
+	sort -u download.tmp >> "$CONFIG_DIR/download.list"
+	sort -u files.tmp >> "$CONFIG_DIR/files.list"
 	
-	#save the download an files lists, remove temp files
-	cat download.tmp >> "$CONFIG_DIR/download.list"
-	cat unique.tmp >> "$CONFIG_DIR/files.list"
+	#Remove temp files
 	cleanup
 }
 
 goodownload() {	
 
 	if [ -f "$CONFIG_DIR/download.list" ]; then
+	
+		#Once again check for duplicates
+		sort -u "$CONFIG_DIR/download.list" > download.tmp
+		mv download.tmp "$CONFIG_DIR/download.list"
+		
 		#download all the files recursively, span domains, replace links with relative local 
 		echo "Beginning file downloads..."	
 		for i in `cat $CONFIG_DIR/download.list` ; do wget -e robots=off --no-verbose -l $WGET_DEPTH -rHk -P $PUBLIC_HTML -D$WGET_DOMAINS "$i";done
@@ -118,7 +122,15 @@ goodownload() {
 		
 		#add downloaded files to download history
 		cat "$CONFIG_DIR/download.list" >> "$CONFIG_DIR/download_history.list"
+		
+		#remove the download queue list and remove duplicates from download_history
 		rm "$CONFIG_DIR/download.list"		
+		sort -u "$CONFIG_DIR/download_history.list" > download_history.tmp
+		mv download_history.tmp "$CONFIG_DIR/download_history.list"
+		
+		#Remove temp files
+		cleanup
+		
 	else
 		echo "Nothing to download.  Try './localgoo.sh init' or './localgoo.sh update' first."
 	fi
@@ -126,10 +138,12 @@ goodownload() {
 
 goonukem() {
 	
+	#Check for duplicates
+	sort -u "$CONFIG_DIR/files.list" > files.tmp
+	mv files.tmp "$CONFIG_DIR/files.list"
+	
 	#replace references in input files with new localgoo using extended regex (-r)
-	if [ -f "$CONFIG_DIR/download.list" ]; then
-		echo "You have downloads queued.  Run './localgoo.sh download' first."
-	elif [ -f "$CONFIG_DIR/files.list" ]; then
+	if [ -f "$CONFIG_DIR/files.list" ]; then
 		#first do a backup
 		goobackup
 
@@ -143,6 +157,14 @@ goonukem() {
 	else
 		echo "No files to fix.  Try './localgoo.sh init' or './localgoo.sh update' first."
 	fi
+	
+	#Check if you need to download stuff
+	if [ -f "$CONFIG_DIR/download.list" ]; then
+		echo "You have downloads queued.  You may want to run './localgoo.sh download'."
+	fi
+	
+	#Remove tmp files
+	cleanup
 }
 
 gooupdate() {
@@ -150,7 +172,7 @@ gooupdate() {
 		#Run the search
 		goosearch
 		
-		#Check for previous downloads
+		#Check for previous downloads and remove them from the queue
 		cat "$CONFIG_DIR/download.list" "$CONFIG_DIR/download_history.list" >> download.tmp
 		sort download.tmp > download2.tmp
 		uniq -u download2.tmp > "$CONFIG_DIR/download.list"
